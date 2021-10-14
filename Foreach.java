18acd1c86c582f34d1df48d1a25dca9abe691e6c
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-10-13 22:56:00 -0300

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,17 @@
+package colecoes;
+
+public class Foreach {
+	
+	public static void main(String[] args) 
+	{
+		
+		double[] valores = {9.4, 2.5, 2.5};
+		
+		
+		for(double valor:valores)
+		{
+			System.out.println(valor);
+		}
+	}
+}
+
